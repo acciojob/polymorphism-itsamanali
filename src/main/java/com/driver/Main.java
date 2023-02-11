da9f772aa5e49379124fc483032bfe0878b04487
Syed Aman Ali <105538460+itsamanali@users.noreply.github.com>
2023-02-11 15:44:29 +0530

    err correct

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public void main(String[] args) {
         Product p = new Product();
 
-        p.product();
-        p.product();
-        p.product();
+        p.product(5,6);
+        p.product(6,3,4);
+        p.product(5.2,5.2);
     }
     class Product{
         public int product(int x, int y) {
