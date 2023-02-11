ebcf998fb2abe5be9afdd4e6725b882ad29560c1
Syed Aman Ali <105538460+itsamanali@users.noreply.github.com>
2023-02-11 15:37:03 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,22 @@ package com.driver;
 
 public class Main {
 
+    public void main(String[] args) {
+        Product p = new Product();
+
+        p.product();
+        p.product();
+        p.product();
+    }
+    class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
