57ced0cdc62f34125276e28efffbd2098b2fe570
Syed Aman Ali <105538460+itsamanali@users.noreply.github.com>
2023-02-11 15:47:44 +0530

    again err

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,21 +2,21 @@ package com.driver;
 
 public class Main {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Product p = new Product();
 
         p.product(5,6);
         p.product(6,3,4);
         p.product(5.2,5.2);
     }
-    class Product{
-        public int product(int x, int y) {
+   static class Product{
+        public static int product(int x, int y) {
             return x*y;
         }
-        public int product(int x, int y, int z) {
+        public static int product(int x, int y, int z) {
             return x*y*z;
         }
-        public double product(double x, double y) {
+        public static double product(double x, double y) {
             return x*y;
         }
     }
